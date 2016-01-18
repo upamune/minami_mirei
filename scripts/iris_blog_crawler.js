15692d090e116e161e3a79e627538b794a1f65e2
upamune <jajkeqos@gmail.com>
2016-01-18 12:02:28 +0900

    :rocket: i☆Risのブログ更新botを追加した

---
+++ scripts/iris_blog_crawler.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var _cheerio = require('cheerio');
+
+var _cheerio2 = _interopRequireDefault(_cheerio);
+
+var _redis = require('redis');
+
+var _redis2 = _interopRequireDefault(_redis);
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _cron = require('cron');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = function (robot) {
+  var url = 'http://ameblo.jp/iris-official-blog';
+  var to = process.env.HUBOT_TWITTER_USER;
+  var client = _redis2.default.createClient();
+
+  new _cron.CronJob('0 * * * * *', function () {
+    _request2.default.get(url, function (err, res, body) {
+      if (!err && res.statusCode === 200) {
+        (function () {
+          var $ = _cheerio2.default.load(body);
+          var $title = $('article .skin-entryHead .skin-entryTitle a');
+
+          var title = $title.text();
+          var link = $title.attr('href');
+          var author = $('article .skin-entryHead .skin-entryThemes a').text();
+
+          client.get(url, function (err, reply) {
+            if (reply === null || title === reply.toString()) {
+              var msg = author + ' ちゃんがブログを更新したぷり!! ' + title + ' - ' + link;
+              robot.reply(to, msg);
+              client.set(url, title);
+            }
+            client.end(true);
+          });
+        })();
+      }
+    });
+  });
+};
---
+++ src/iris_blog_crawler.js
@@ -0,0 +1,33 @@
+import cheerio from 'cheerio';
+import redis from 'redis';
+import request from 'request';
+import { CronJob as cron } from 'cron';
+
+module.exports = (robot) => {
+  let url = 'http://ameblo.jp/iris-official-blog';
+  let to = process.env.HUBOT_TWITTER_USER;
+  let client = redis.createClient();
+
+  new cron('0 * * * * *', () => {
+    request
+    .get(url, (err, res, body) => {
+      if(!err && res.statusCode === 200) {
+        let $ = cheerio.load(body);
+        let $title = $('article .skin-entryHead .skin-entryTitle a');
+
+        let title = $title.text();
+        let link = $title.attr('href');
+        let author = $('article .skin-entryHead .skin-entryThemes a').text();
+
+        client.get(url, (err, reply) => {
+          if(reply === null || title === reply.toString()) {
+            let msg = `${author} ちゃんがブログを更新したぷり!! ${title} - ${link}`;
+            robot.reply(to, msg);
+            client.set(url, title);
+          }
+          client.end(true);
+        });
+      }
+    });
+  });
+};
